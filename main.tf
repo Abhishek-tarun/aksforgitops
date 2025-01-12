1445b549978ac85d53286fc164df5890a0fcbe5f
Abhishek Mishra <80722398+Abhishek-tarun@users.noreply.github.com>
2025-01-12 20:34:40 +0100

    AKS Cluster with Terraform

---
+++ main.tf
@@ -0,0 +1,24 @@
+##AKS Cluster setup
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+    name = "aks_cluster"
+    location = azurerm_resource_group.rg-aks.location
+    resource_group_name = azurerm_resource_group.rg-aks.name
+    dns_prefix = "aks_dns"
+
+    default_node_pool {
+      name = "default"
+      node_count = 1
+      vm_size = "Standard_DS2_v2"
+      vnet_subnet_id = azurerm_subnet.aks_subnet.id
+
+    }
+}
+
+##identity block
+identity {
+    type = "SystemAssigned"
+}
+
+tags = {
+    Environment = "dev"
+}   
---
+++ network.tf
@@ -0,0 +1,14 @@
+##Creating virtual nwtwork
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "aks-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg-aks.location
+  resource_group_name = azurerm_resource_group.rg-aks.name 
+}
+
+##Creating subnet
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg-aks.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["
