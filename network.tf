75072e0ebd2e2016dedc36f0aeed7d81f84c1ce4
Abhishek Mishra <80722398+Abhishek-tarun@users.noreply.github.com>
2025-01-12 20:37:09 +0100

    Correction in network.tf file

--- network.tf
+++ network.tf
@@ -11,4 +11,5 @@ resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.rg-aks.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["
+  address_prefixes     = ["10.1.1.0/16"]
+}
